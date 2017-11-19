dec6205d0945f1cbb21da3acc6d188b9a2643d75
Haoxuan Zhang <jeremysk66@gmail.com>
2017-11-19 15:25:05 +1100

    create 1.py

---
+++ wechat_sc.py
@@ -0,0 +1,121 @@
+#使用selenium中webdriver驱动chrome来进行浏览器模拟
+#同时添加报头 验证身份
+#但获取的仅仅是链接 未能提取有效数据
+
+from selenium import webdriver
+import time
+import json
+from pprint import pprint
+
+import csv
+
+
+post = {}
+
+driver = webdriver.Chrome(executable_path='/Users/jeremyzhang/Downloads/chromedriver')
+driver.get('https://mp.weixin.qq.com/')
+time.sleep(2)
+driver.find_element_by_xpath("./*//input[@name='account']").clear()
+driver.find_element_by_xpath("./*//input[@name='account']").send_keys('445686691@qq.com')
+#send keys value should be the id of your wechat id
+driver.find_element_by_xpath("./*//input[@name='password']").clear()
+driver.find_element_by_xpath("./*//input[@name='password']").send_keys('Jeremy_1994')
+#send keys value should be your password
+# Click remember me
+time.sleep(5)
+driver.find_element_by_xpath("./*//a[@title='点击登录']").click()
+# scan the code
+time.sleep(20)
+driver.get('https://mp.weixin.qq.com/')
+cookie_items = driver.get_cookies()
+for cookie_item in cookie_items:
+    post[cookie_item['name']] = cookie_item['value']
+cookie_str = json.dumps(post)
+with open('cookie.txt', 'w+', encoding='utf-8') as f:
+    f.write(cookie_str)
+
+import requests
+import redis
+import json
+import re
+import random
+import time
+
+
+gzlist = ['yq_Butler']
+
+url = 'https://mp.weixin.qq.com'
+header = header = {
+    "HOST": "mp.weixin.qq.com",
+    "User_Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
+}
+
+with open('cookie.txt', 'r', encoding='utf-8') as f:
+    cookie = f.read()
+cookies = json.loads(cookie)
+response = requests.get(url=url, cookies=cookies)
+token = re.findall(r'token=(\d+)', str(response.url))[0]
+for query in gzlist:
+    query_id = {
+        'action': 'search_biz',
+        'token' : token,
+        'lang': 'zh_CN',
+        'f': 'json',
+        'ajax': '1',
+        'random': random.random(),
+        'query': 'dianping_sydney',
+        # 'kebeiquan' is a wechat id
+        'begin': '0',
+        'count': '5',
+    }
+    search_url = 'https://mp.weixin.qq.com/cgi-bin/searchbiz?'
+    search_response = requests.get(search_url, cookies=cookies, headers=header, params=query_id)
+    lists = search_response.json().get('list')[0]
+    fakeid = lists.get('fakeid')
+    query_id_data = {
+        'token': token,
+        'lang': 'zh_CN',
+        'f': 'json',
+        'ajax': '1',
+        'random': random.random(),
+        'action': 'list_ex',
+        'begin': '0',
+        'count': '5',
+        'query': '',
+        'fakeid': fakeid,
+        'type': '9'
+    }
+    appmsg_url = 'https://mp.weixin.qq.com/cgi-bin/appmsg?'
+    appmsg_response = requests.get(appmsg_url, cookies=cookies, headers=header, params=query_id_data)
+    max_num = appmsg_response.json().get('app_msg_cnt')
+    num = int(int(max_num) / 5)
+    begin = 250
+    while num + 1 > 0 :
+        query_id_data = {
+            'token': token,
+            'lang': 'zh_CN',
+            'f': 'json',
+            'ajax': '1',
+            'random': random.random(),
+            'action': 'list_ex',
+            'begin': '{}'.format(str(begin)),
+            'count': '5',
+            'query': '',
+            'fakeid': fakeid,
+            'type': '9'
+        }
+        print('翻页###################',begin)
+        query_fakeid_response = requests.get(appmsg_url, cookies=cookies, headers=header, params=query_id_data)
+        fakeid_list = query_fakeid_response.json().get('app_msg_list')
+        for item in fakeid_list:
+            print(item.get('link'))
+            url = [item.get('link')]
+        num -= 1
+        begin = int(begin)
+        begin+=5
+        time.sleep(8)
+print (url)
+
+c = open('url1.csv','w')
+writer=csv.writer(c)
+writer.writerow(url)
